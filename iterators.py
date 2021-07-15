a64b36acb3253e6d1a2846b9be5a4141e2395c6c
Luisf543678 <luisf543678@gmail.com>
2021-07-14 23:36:58 -0500

    adding file class 1

---
+++ iterators.py
@@ -0,0 +1,19 @@
+
+
+my_list = [1,2,3,4,5,6,7,8,9,10]
+
+#for element in my_list:
+#    print(element)
+
+#print(type(element))
+
+my_iter = iter(my_list)
+
+# Extraer los elementos
+
+print(next(my_iter))
+
+
+
+
+
