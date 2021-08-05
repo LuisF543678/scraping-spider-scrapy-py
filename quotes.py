f265b03d4b07253ef2e73e2d53ee67534c96163f
Luisf543678 <luisf543678@gmail.com>
2021-08-05 03:21:20 -0500

    add extract data

--- quotes.py
+++ quotes.py
@@ -1,17 +1,38 @@
-import scrapy 
+import scrapy
+
+#   Title = //h1/a/text()
+#   Citas = //span[@class="text" and @Itempro="text"]/text()
+#   Top ten tag = response.xpath('//div/[contains(@class, "tags-box")]//span[@class="tag-item"]/a/text()')getall()
+
 
 class QuotesSpider(scrapy.Spider):
-    name = 'quotes'
-    start_url = [
-        'http://quotes.toscrape.com/'
-    ]
+    name = "quotes"
+    start_urls = ["https://quotes.toscrape.com/"]
 
     def parse(self, response):
-        print('*' * 30)
-        print('\n\n')
-        print(response.status, response.headers)
-        print('\n\n')
-        print('*' * 30)
-        print('\n\n')
+        print("*" * 30)
+        print("\n\n\n")
+
+        Title = response.xpath('//h1/a/text()').get()
+        print(f'Titulo: {Title}')
+        print("\n\n\n")
+
+
+        quotes = response.xpath('//span[@class="text" and @itemprop="text"]/text()').getall()
+        print(f'Citas: ')
+        for quote in quotes:
+            print(f'- {quote}')
+        print("\n\n\n")
+
+
+        top_ten_tags = response.xpath('//div[contains(@class, "tags-box")]//span[@class="tag-item"]/a/text()').getall()
+        print('Top ten tags')
+        for tag in top_ten_tags:
+            print(f'{tag}')
+
+        #   print(response.status, response.headers)
+        print("\n\n")
+        print("*" * 30)
+        print("\n\n")
 
 #Console scrapy crawl quotes 
---
+++ scrapy.py
@@ -0,0 +1,6 @@
+
+
+## Console    scrapy shell 'https://quotes.toscrape.com/'
+
+#   response.xpath('//div/[ contains(@class, "tags-box")]//span[@class="tag-item"]/a/text()')getall()
+
